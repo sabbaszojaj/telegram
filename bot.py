87d28d4aeab1e7ad659d928f9dde1f4f912a6e33
sabbaszojaj <sabbaszojaji@gmail.com>
2025-07-20 10:46:00 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 from telegram.ext import Updater, CommandHandler
 import os
 
+# گرفتن توکن از متغیر محیطی
 TOKEN = os.environ.get("BOT_TOKEN")
 
+# تابع برای پاسخ به دستور /start
 def start(update, context):
     update.message.reply_text("سلام! ربات زنده است و آماده پاسخگویی 😊")
 
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
+
+    # اضافه کردن هندلر برای دستور /start
     dp.add_handler(CommandHandler("start", start))
+
+    # شروع ربات
     updater.start_polling()
     updater.idle()
 
